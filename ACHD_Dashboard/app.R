54d7499e06f0286118605f23b204c199e8bd31fc
Calum <calumanicholson@gmail.com>
2021-04-04 22:19:42 +1000

    fixed a few file paths for app deployment

--- ACHD_Dashboard/app.R
+++ ACHD_Dashboard/app.R
@@ -19,7 +19,6 @@ library(sf)
 library(leaflet)
 library(leaflet.extras)
 library(tableHTML)
-library(here)
 
 
 # Path to ACHD database data (note: only accessible at RPAH)
@@ -31,21 +30,21 @@ library(here)
 # import ACHD data
 # In this public version this is a limit dataset that is aggregated to area level informaton
 # rather than patient level information
-achd <- readRDS(here("ACHD_Dashboard", "data", "area_achd.rds"))
+achd <- readRDS("./data/area_achd.rds")
 
 #import DX coding
-dx_codes <- read.csv(file = here("ACHD_Dashboard", "data", "ACHD_EPCC_coding.csv"), fileEncoding="UTF-8-BOM") %>% 
+dx_codes <- read.csv(file = "./data/ACHD_EPCC_coding.csv", fileEncoding="UTF-8-BOM") %>% 
             filter(!(Variable == 'adm_AbsentPA' | Variable == 'PA' | Variable == "achd_id")) %>%
             select(!c(ACHD_Database_name, check., Variable))
 
 #import Census data
-sa2.TB <- readRDS(file = here("ACHD_Dashboard", "data", "sa2_demographics.rds")) %>%
+sa2.TB <- readRDS(file = "./data/sa2_demographics.rds") %>%
                             mutate(IRSD = ordered(IRSD, c(1,2,3,4,5,6,7,8,9,10)),
                                    IRSAD = ordered(IRSAD, c(1,2,3,4,5,6,7,8,9,10)))
 
 #import htt data
-htt.nsw <- readRDS(file = here("ACHD_Dashboard", "data", "htt_nsw.rds")) # Driving times data
-htt.details <- readRDS(file = here("ACHD_Dashboard", "data", "htt_details.rds")) # Hospital Metadata
+htt.nsw <- readRDS(file = "./data/htt_nsw.rds") # Driving times data
+htt.details <- readRDS(file = "./data/htt_details.rds") # Hospital Metadata
 
 #current achd clinics
 achd_ids <- c(152, 408, 646, 683, 737, 979)
@@ -53,7 +52,7 @@ achd_ids <- c(152, 408, 646, 683, 737, 979)
 new_achd_ids <- achd_ids
 
 #Import area polygons
-sa2.polys <- readOGR(here("ACHD_Dashboard", "data", "ASGS", "sa2", "sa2_polys.shp"))
+sa2.polys <- readOGR("./data/ASGS/sa2/sa2_polys.shp")
 
 ############################# HEADER CONTENT #######################################
 
